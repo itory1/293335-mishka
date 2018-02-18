7323d0ccb2b2e7cedee9e421989d4712dd66b618
Victoria Ivanova <i_tory@mail.ru>
2018-02-18 13:26:02 +0300

    Доделываю все страницы, ошибка в сборщике

--- gulpfile.js
+++ gulpfile.js
@@ -15,16 +15,16 @@ var server = require("browser-sync").create();
 var run = require("run-sequence");
 
 
-gulp.task("style", function() {
+gulp.task("style", function () {
   gulp.src("source/less/style.less")
     .pipe(plumber())
     .pipe(less())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("build/source/css"))
-    .pipe(minify())
-    .pipe(rename("style.min.css"))
+    //    .pipe(gulp.dest("build/source/css"))
+    //    .pipe(minify())
+    //    .pipe(rename("style.min.css"))
     .pipe(gulp.dest("build/source/css"))
     .pipe(server.stream());
 
@@ -32,8 +32,10 @@ gulp.task("style", function() {
 
 gulp.task("html", function () {
   return gulp.src("source/*.html")
-  .pipe(posthtml())
-  .pipe(gulp.dest("build"));
+//    .pipe(posthtml([
+//    include()
+//  ]))
+    .pipe(gulp.dest("build"));
 });
 
 gulp.task("images", function () {
@@ -42,17 +44,19 @@ gulp.task("images", function () {
     imagemin.optipng({optimizationLevel: 3}),
     imagemin.jpegtran({progressive: true}),
     imagemin.svgo()
-    ]))
-  .pipe(gulp.dest("build/source/img"));
+ ]))
+    .pipe(gulp.dest("build/source/img"))
 });
 
 gulp.task("webp", function () {
   return gulp.src("source/img/**/*.{png,jpg}")
-  .pipe(webp({quality: 90}))
-  .pipe(gulp.dest("build/source/img"));
- });
+    .pipe(webp({
+      quality: 90
+    }))
+    .pipe(gulp.dest("build/source/img"));
+});
 
-gulp.task("serve", function() {
+gulp.task("serve", function () {
   server.init({
     server: "build/",
     notify: false,
@@ -61,32 +65,34 @@ gulp.task("serve", function() {
     ui: false
   });
 
-  gulp.watch("source/less/**/*.less", ["style"]);
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/less/**/*.less", ["style"]).on("change", server.reload);
+  gulp.watch("*.html").on("change", server.reload);
 });
 
 gulp.task("copy", function () {
   return gulp.src([
-  "source/fonts/**/*.{woff,woff2}",
-  "source/img/**",
-  "source/js/**",
-  "source/*.html"
+  "fonts/**/*.{woff,woff2}",
+  "img/**",
+  "js/**",
+  "*.html"
   ], {
-  base: "source"
-  })
-  .pipe(gulp.dest("build"));
- });
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
+});
 
- gulp.task("clean", function () {
+gulp.task("clean", function () {
   return del("build");
- });
+});
 
 gulp.task("build", function (done) {
   run(
     "clean",
     "copy",
     "style",
+    "images",
+    "webp",
     "html",
     done
-  );
+  )
 });
